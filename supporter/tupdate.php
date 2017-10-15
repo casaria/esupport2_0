a6c78ee82f9cf7a145bd13a2f7f078429a7b0228
Peter Horrack <phorrack@casaria.net>
2017-10-15 09:02:19 -0700

    showformattedtime, some common upgrades to displayticket

--- supporter/tupdate.php
+++ supporter/tupdate.php
@@ -421,11 +421,9 @@ echo "<form name=form1 method=post enctype=\"multipart/form-data\">";
 		echo '<a href="updatelog.php?cookie_name='.$cookie_name.'&id='.$info['id'].'" target="myWindow" onClick="window.open(\'\', \'myWindow\',
 					\'location=no, status=yes, scrollbars=yes, height=500, width=600, menubar=no, toolbar=no, resizable=yes\')">
 					<img border=0 src="../'.$theme['image_dir'].'orangeglow0_show_summary.png"></a> ';
-        echo '<a href="updatelog.php?cookie_name='.$cookie_name.'&id='.$info['id'].'" target="myWindow" onClick="window.open(\'\', \'myWindow\',
-					\'location=no, status=yes, scrollbars=yes, height=500, width=600, menubar=no, toolbar=no, resizable=yes\')">
-					<img border=0 src="../'.$theme['image_dir'].'orangeglow0_show_summary.png"></a> ';
+        echo "<div align=right><a href=\"$supporter_site_url/print.php?id=$id\">$lang_printable</a></div>";
 
-		echo "<div align=right><a href=\"$supporter_site_url/print.php?id=$id\">$lang_printable</a></div>";
+
 		createTicketDetails();
 		createSupporterInfo();
 		createUserInfo();

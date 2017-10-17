88119f3c30a092ce063fc8a4767fc6a3e0be29f3
Peter Horrack <phorrack@casaria.net>
2017-10-16 23:58:23 -0700

    configuration JS-GRID

--- jsgrid-php/db/config.php
+++ jsgrid-php/db/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    "db" => "mysql:host=127.0.0.1;dbname=casaria_hdesk1",
-    "username" => "hostgo_hdesk1",
+    "db" => "mysql:host=127.0.0.1;dbname=hostgo_hdesk1",
+    "username" => "casaria_hdesk1",
     "password" => "5XwoR]B"
 );
 

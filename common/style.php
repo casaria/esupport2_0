5812bfe385df8f1f126364d8563c5cb7f5dd42db
Peter Horrack <phorrack@casaria.net>
2017-10-20 03:46:48 -0700

    style 700-600

--- common/style.php
+++ common/style.php
@@ -108,7 +108,7 @@ $tablePadding = 6;
         #selecwidth, selectwidth2 option{
             width:375px; font-weight:600;
             color: darkslateblue;
-            line-height: 2;
+            line-height: 50px;
             font-size: 13px;
         }
         BODY {background: <?php echo $theme['bgcolor'];?> ; color: black; font-weight:600;}

83a71b4803fa1ebd3a971bd060f92d70089ed2ee
Peter Horrack <phorrack@casaria.net>
2017-10-21 13:50:10 -0700

    button sumbmit

--- common/style.php
+++ common/style.php
@@ -67,7 +67,7 @@ $tablePadding = 6;
     <TITLE> <?php echo $helpdesk_name;?></TITLE>
     <?php
     if ($theme['font'] == "Titillium Web") {
-        $lineHeight = 1.1;
+        $lineHeight =0.8;
         $tablePadding = 5;
     } else  {
         $lineHeight = 1.1;

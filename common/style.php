657935e9251e03e02a7bf450b52dde46c9095247
Peter Horrack <phorrack@casaria.net>
2017-10-22 16:05:04 -0700

    bootstrap padding, borderspacing overrides

--- common/style.php
+++ common/style.php
@@ -102,9 +102,6 @@ $tablePadding = 6;
                 margin: 1px;
             }
 
-            ul {
-                padding-left: 4px;
-            }
 
             @import url(https://fonts.googleapis.com/css?family=Lato);
 

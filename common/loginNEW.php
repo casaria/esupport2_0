36d7d849ea0be0afc5425334f30d3628565d9a8e
Peter Horrack <phorrack@casaria.net>
2017-10-23 06:36:04 -0700

    Login styled

--- common/loginNEW.php
+++ common/loginNEW.php
@@ -181,6 +181,16 @@ echo'
   <TR>
     <TD>
     
+    <div class="card">
+  <div class="header">
+    <h1>1</h1>
+  </div>
+
+  <div class="container">
+    <p>January 1, 2016</p>
+  </div>
+</div>
+    
     </TD></TR><TR><TD>
       <TABLE style="margin:1px; Padding:1px; width=100%; -webkit-box-shadow: #797991 3px 3px 3px;
             -moz-box-shadow: #797991 3px 3px 3px;

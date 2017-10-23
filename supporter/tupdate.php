5715101f15db1102699a8d7a7ec130be7d8e39f4
Peter Horrack <phorrack@casaria.net>
2017-10-23 12:48:43 -0700

    bootstrap on EXTRA TAB

--- supporter/tupdate.php
+++ supporter/tupdate.php
@@ -874,6 +874,7 @@ function extraTab() {
 
 
     <div class="progress">
+        <P><H4>TICKET UPDATE performance (onn time percentage)</H4> </P>
         <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
             <span class="sr-only">40% Complete (success)</span>
         </div>

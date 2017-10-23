309f6d99face2fc0ec57704c6a76fc82045405e0
Peter Horrack <phorrack@casaria.net>
2017-10-23 12:33:59 -0700

    bootstrap on EXTRA TAB

--- supporter/tupdate.php
+++ supporter/tupdate.php
@@ -873,7 +873,7 @@ function extraTab() {
     ?>
 
 
-    <a tabindex="0" class="btn btn-lg btn-danger" role="button" data-toggle="popover" data-trigger="focus" title="Dismissible popover" data-content="And here's some amazing content. It's very engaging. Right?">Dismissible popover</a>
+    <button type="button" class="btn btn-lg btn-danger" data-toggle="popover" title="Popover title" data-content="And here's some amazing content. It's very engaging. Right?">Click to toggle popover</button>
 
 
   <script>

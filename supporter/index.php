c4e0c3d12b630ddd489ecc2babaf954370d3c491
Peter Horrack <phorrack@casaria.net>
2017-10-25 03:47:06 -0700

    Printable stats $hidemenu declarartion and assignment

--- supporter/index.php
+++ supporter/index.php
@@ -90,7 +90,7 @@ $enable_CloudControl = getCloudControlUserSetting($_SESSION['cookie_name']);
 
 ?>
 <BODY class=body>
-<TABLE class=border cellSpacing=0 cellPadding=0 width="<?php echo $theme['width']; ?>" align=center 
+<TABLE class=border cellSpacing=2 cellPadding=0 width="<?php echo $theme['width']; ?>" align=center
 border=0>
   <TBODY> 
   <TR> 

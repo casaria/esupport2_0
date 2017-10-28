11eb35bfd7819f55cf4619c7e9c7dbe796632b42
Peter Horrack <phorrack@casaria.net>
2017-10-28 05:55:20 -0700

    switched database configuration to clone_hdesk1

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -172,45 +172,7 @@ else {
             <div id="scan" class="tab-pane fade">
             <h3>SCAN</h3>
             <p>This is where the serviced asset is scanned or selected
-            <div class="row">
-            <div class="col-xs-6">
-                <div class="input-group">
-                    <input id="scanner_input" class="form-control" placeholder="Click the button to scan an EAN..."
-                           type="text"/>
-                    <span class="input-group-btn">
-                                        <button class="btn btn-default" height="50" type="button" data-toggle="modal"
-                                                data-target="#livestream_scanner">
-                                        <i class="fa fa-barcode"></i>
-                                    </button>
-                                   </span>
-                </div><!-- /input-group -->
-            </div><!-- /.col-lg-6 -->
-        </div><!-- /.row -->
-        <div class="modal" id="livestream_scanner">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                        <h4 class="modal-title">Barcode Scanner</h4>
-                    </div>
-                    <div class="modal-body" style="position: static">
-                        <div id="interactive" class="viewport"></div>
-                        <div class="error"></div>
-                    </div>
-                    <div class="modal-footer">
-                        <label class="btn btn-default pull-left">
-                            <i class="fa fa-camera"></i> Use camera app
-                            <input type="file" accept="image/*;capture=camera" capture="camera" class="hidden"/>
-                        </label>
-                        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-                    </div>
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
-
-
+            <?php createScanpage(); ?>
         </p>
     </div>
     <div id="extra" class="tab-pane fade">
@@ -252,6 +214,49 @@ else {
     </div>
     <?php
 }
+
+function createScanpage(){
+
+ echo '
+    <div class="row">
+	<div class="col-lg-6">
+		<div class="input-group">
+			<input id="scanner_input" class="form-control" placeholder="Click the button to scan an EAN..." type="text" /> 
+			<span class="input-group-btn"> 
+				<button class="btn btn-default" type="button" data-toggle="modal" data-target="#livestream_scanner">
+					<i class="fa fa-barcode"></i>
+				</button> 
+			</span>
+		</div><!-- /input-group -->
+	</div><!-- /.col-lg-6 -->
+</div><!-- /.row -->
+<div class="modal" id="livestream_scanner">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title">Barcode Scanner</h4>
+			</div>
+			<div class="modal-body" style="position: static">
+				<div id="interactive" class="viewport"></div>
+				<div class="error"></div>
+			</div>
+			<div class="modal-footer">
+				<label class="btn btn-default pull-left">
+					<i class="fa fa-camera"></i> Use camera app
+    <input type="file" accept="image/*;capture=camera" capture="camera" class="hidden" />
+				</label>
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+';
+}
+
+
 
 function createSupporterInfo()
 {

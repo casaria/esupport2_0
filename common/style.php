10f03134f06b5c21d6c88064fb6848f2ba9d5cea
Peter Horrack <phorrack@casaria.net>
2017-10-30 02:34:37 -0700

    index.php updates

--- common/style.php
+++ common/style.php
@@ -45,7 +45,7 @@ $tablePadding = 6;
 
 
 
-        <link rel="stylesheet" href="/support/common/bootstrap.css" media="screen">
+        <link rel="stylesheet" href="../common/bootstrap.css" media="screen">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 

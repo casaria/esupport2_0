3d6e4e8118d31533a53c0ace30d645ed0be548ea
Peter Horrack <phorrack@casaria.net>
2017-10-30 02:13:47 -0700

    index.php updates

--- supporter/index.php
+++ supporter/index.php
@@ -29,7 +29,7 @@
 			**
 			***************************************************************************************/
 
-require_once "../common/config.php";
+    require_once "../common/config.php";
 require_once "../common/mysql.class.php";
 require_once "../common/common.php";
 
@@ -39,7 +39,7 @@ if($SERVER_PORT == 80 && $enable_ssl == 'On' && (!isset($cookie_name) || $cookie
 }
                   
 require "../common/login.php";
-//RewindSession();
+RewindSession();
 ?>
 
 <script type="text/javascript">

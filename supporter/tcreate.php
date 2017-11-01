a2a1386c44e13f6b8c5b25479226b0af01481139
Peter Horrack <phorrack@casaria.net>
2017-11-01 05:10:39 -0700

    tcreate update

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -176,10 +176,7 @@ else {
             </div>
             <div id="extra" class="tab-pane fade">
                 <h4>EXTRA</h4>
-                <H4>Additional ticket details (PO/Customer reference, cost centers, time budget, due time, continuation/follow up </H44>
-            </div>
-
-
+                <H4>Additional ticket details (PO/Customer reference, cost centers, time budget, due time, continuation/follow up </H4>
             <div id="asset" class="tab-pane fade">
                 <h4>ASSET</h4>
                 <H4>Asset sepcific, preconfigured defaults, this tab allows modification.</H4>

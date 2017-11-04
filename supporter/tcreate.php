65cf5dfb730717907496997e8b99d30d52932659
Peter Horrack <phorrack@casaria.net>
2017-11-04 05:13:29 -0700

    Transition to Bootstrap 4.0

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -139,7 +139,7 @@ else {
 
 
     <div class="container">
-        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <ul class="nav nav-tabs" id="myTab" role="tablist">
             <li class="nav-item">
                 <a class="nav-link active" id="new-tab" data-toggle="tab" href="#new" role="tab" aria-controls="new" aria-selected="true"><h3>NEW</h3></a>
             </li>

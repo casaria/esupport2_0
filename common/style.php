73877581a57359d95c87766148faaa94593353c6
Peter Horrack <phorrack@casaria.net>
2017-11-04 00:07:33 -0700

    Cleaned stage commit Scnaning works in all browesers, All libraries are loaded , Dylay works Angular? JSgrid ? Username issues with spaces and cpaitalization is fixed

--- common/style.php
+++ common/style.php
@@ -60,7 +60,7 @@ $tablePadding = 6;
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-
+    <link href='https://fonts.googleapis.com/css?family=Titillium Web:300:400' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/dylay/assets/css/main.css" media="screen">
 
 

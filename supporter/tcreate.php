9fe9527e6feec72d14c147b69dfe1a2a295d58ea
Peter Horrack <phorrack@casaria.net>
2017-11-05 02:00:02 -0800

    Tryoing NAV pills

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -139,11 +139,6 @@ else {
 
 
 
-<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-    <li class="nav-item">
-        <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
-    </li>
-
 
 
     <div class="container">

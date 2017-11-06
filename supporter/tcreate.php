5a6fd8bd78dae2203f25f6c896d359b69a4a875e
Peter Horrack <phorrack@casaria.net>
2017-11-05 17:23:36 -0800

    bootstrap patched

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -209,14 +209,12 @@ function createScanpage(){
 
  echo '
     <div class="row">
-	<div class="col-xs-4`">
+	<div class="col-xs-6">
 		<div class="input-group">
 		<span class="input-group-btn"> 
-			<input id="scanner_input" class="text-tag"  placeholder="SCAN Casaria TAG #" type="text" /> 
+			<input id="scanner_input" class="text-tag"  placeholder="SCAN/ENTER CASTAG#..." type="text" /> 
 			
-				<button class="btn btn-default" type="button" data-toggle="modal" data-target="#livestream_scanner">
-				
-		        <button class="btn-scan" type="button" data-toggle="modal" data-target="#livestream_scanner">SCAN
+				<button class="btn-scan" type="button" data-toggle="modal" data-target="#livestream_scanner">SCAN
 					<i class="fa fa-barcode"></i>
 				</button> 
 			</span>

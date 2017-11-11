bbcf6fe098c7a0cee110934517c4be61a94a0f7f
Peter Horrack <phorrack@casaria.net>
2017-11-11 02:02:20 -0800

    Billingstatus fixed

--- common/style.php
+++ common/style.php
@@ -1073,6 +1073,7 @@ console.log(document.querySelector('meta[name="viewport"]'));
                     outline: 0;
                     overflow: hidden;
                     padding: 2px 2px 2px 2px;
+                    font-stretch: expanded;
 
                     /* background: transparent;
                      padding: 5px 10px 5px 5px;

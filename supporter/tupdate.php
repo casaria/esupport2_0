2bec381bb5ef02a332408c2a758a8e9c9ee999c1
Peter Horrack <phorrack@casaria.net>
2017-11-16 05:20:47 -0800

    tupdate RESTORED

--- supporter/tupdate.php
+++ supporter/tupdate.php
@@ -454,7 +454,7 @@ unset($update);
 
 
         <?php
-        echo "<div class=\"row\"w>";
+        echo "<div class=\"row\">";
         echo "<input class='col-xs-2' type=hidden name=sg value='" . $sg . "'>";
         echo "<input type=hidden name=id value='" . $info['id'] . "'>";
         echo "<input type=hidden name=old_supporter value='" . $info['supporter'] . "'>";

9b9dae64dda6c656322fd7ef91acab7d09039096
Peter Horrack <phorrack@casaria.net>
2017-11-16 07:02:09 -0800

    Release 1.5.1b

--- supporter/tupdate.php
+++ supporter/tupdate.php
@@ -465,7 +465,7 @@ unset($update);
         echo "<input type=hidden name=old_emailcc value='" . $info['emailcc'] . "'>";
         echo "<input type=hidden name=old_status value='" . $info['status'] . "'>";
         echo "<input type=hidden name=minutes_labor value=$minutes>";
-        echo "<div class=\"col-4\">< == nput type=submit id=submit name=update height=80 value=\"$lang_updateticket\">";
+        echo "<div class=\"col-4\"><input type=submit id=submit name=update height=80 value=\"$lang_updateticket\">";
         echo "</div></form";
 
 
@@ -478,7 +478,7 @@ unset($update);
         echo "<input type=hidden name=description value='$info[description]'>";
         echo "<div class=\"col-4\"><input type=submit id=submit name=dumptokb height=80 value=\"$lang_dumptokb\"></div></form>";
         }
-        Echo "</div>"; // row
+        Echo "</div>";
 
 
 

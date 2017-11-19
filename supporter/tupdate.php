80ad6c63a84b8ff608b9d1a90fe106dfc7c25202
Peter Horrack <phorrack@casaria.net>
2017-11-18 20:13:16 -0800

    Porgeress bars AFTR_HRS checkbox working

--- supporter/tupdate.php
+++ supporter/tupdate.php
@@ -1027,32 +1027,18 @@ function createTimeTab()
 
             ?>
             <div class="progress">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 89%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress">
+                <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress">
+                <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div class="progress">
+                    <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
 
-                <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar"
-                     aria-valuenow="40"
-                     aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                    <span class="sr-only">40% Complete (success)</span>
-                </div>
-            </div>
-            <div class="progress">
-                <div class="progress-bar progress-bar-info progress-bar-striped" role="progressbar" aria-valuenow="20"
-                     aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                    <span class="sr-only">20% Complete</span>
-                </div>
-            </div>
-            <div class="progress">
-                <div class="progress-bar progress-bar-warning progress-bar-striped" role="progressbar"
-                     aria-valuenow="60"
-                     aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                    <span class="sr-only">60% Complete (warning)</span>
-                </div>
-            </div>
-            <div class="progress">
-                <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="80"
-                     aria-valuemin="0" aria-valuemax="100" style="width: 80%">
-                    <span class="sr-only">80% Complete (danger)</span>
-                </div>
-            </div
 
             <?php
 

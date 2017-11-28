0aea1967ac32d1d8a5ee18333e0ecf70301b6b55
Peter Horrack <phorrack@casaria.net>
2017-11-27 22:54:00 -0800

    newPassword

--- common/mdb22.php
+++ common/mdb22.php
@@ -175,6 +175,14 @@
 
 
 <!-- SCRIPTS -->
+<!-- SCRIPTS -->
+<!-- JQuery -->
+<script type="text/javascript" src="../mdb/js/jquery-3.2.1.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="../mdb/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="../mdb/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
 <script type="text/javascript" src="../mdb/js/mdb.min.js"></script>
 <script>
     new WOW().init();

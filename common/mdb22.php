058669ee8fea0890777a9d357f07bed719f5c951
Peter Horrack <phorrack@casaria.net>
2017-11-27 22:56:32 -0800

    newPassword

--- common/mdb22.php
+++ common/mdb22.php
@@ -10,8 +10,12 @@
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="../css/mdb.min.css" rel="stylesheet">
-
+    <!-- Bootstrap core CSS -->
+    <link href="../mdb/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="../mdb/css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <link href="../mdb/css/style.css" rel="stylesheet">
     <style>
 
         .intro-2 {

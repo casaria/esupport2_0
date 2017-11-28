c043e4b4c9d8916e5311146e00781790aecc94e4
Peter Horrack <phorrack@casaria.net>
2017-11-27 21:01:19 -0800

    PGConnection added

--- common/newPassword.php
+++ common/newPassword.php
@@ -10,9 +10,9 @@ $mtime1 = explode(" ", microtime());
 $starttime = $mtime1[0] + $mtime1[1];
 
 
-require_once "common/config.php";
-require_once "common/$database.class.php";
-require_once "common/common.php";
+require_once "config.php";
+require_once "$database.class.php";
+require_once "common.php";
 
 if($enable_helpdesk == 'Off'){
     printerror($on_off_reason);

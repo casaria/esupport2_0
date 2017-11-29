bb14dcf5b08142f5c1cc9256ca729bd17d3e5993
Peter Horrack <phorrack@casaria.net>
2017-11-29 05:16:44 -0800

    login colors

--- common/login20.php
+++ common/login20.php
@@ -131,23 +131,20 @@ if($enable_helpdesk == 'Off'){
 
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
         <div class="container">
-            <a class="navbar-brand" href="#"><strong>eSupport</strong></a>
+            <a class="navbar-brand" href="#"><strong>eSupport 2.0</strong></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Security <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Security information <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">password</a>
+                        <a class="nav-link" href="#">password guidelines</a>
                     </li>
 
                 </ul>
-                <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="text" placeholder="" aria-label="Search">
-                </form>
             </div>
         </div>
     </nav>

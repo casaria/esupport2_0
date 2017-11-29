8d68cc8eb0ed5ac97897056335783abd5695d315
Peter Horrack <phorrack@casaria.net>
2017-11-29 03:01:09 -0800

    login colors

--- common/style.php
+++ common/style.php
@@ -59,14 +59,17 @@ $tablePadding =2;
     <!--  <script src="../js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="/css/bootstrap.min.css" >
+
+
+
+    <!-- <link rel="stylesheet" href="/css/bootstrap.min.css" > -->
 
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 
-    <!-- THeSE ARE WORKING (ll 4 sources   -->
+    <!-- THeSE ARE WORKING (All 4 sources   -->
 
     <!--  <script src="../dylay/assets/vendor/jquery.easing.1.3.js"></script> -->
     <script src="../dylay/src/dylay.js"></script>
@@ -91,7 +94,9 @@ $tablePadding =2;
     <!-- Material Design Bootstrap -->
     <link href="../mdb/css/mdb.min.css" rel="stylesheet">
     <!-- Your custom styles (optional) -->
-    <link href="../mdb/css/style.css" rel="stylesheet">
+    <!-- <link href="../mdb/css/style.css" rel="stylesheet">
+    -->
+
 
 
 

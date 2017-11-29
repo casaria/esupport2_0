9ae08ac3cd7b31acaac827798a0efe68cce3ad67
Peter Horrack <phorrack@casaria.net>
2017-11-29 05:14:53 -0800

    login colors

--- common/login20.php
+++ common/login20.php
@@ -138,17 +138,15 @@ if($enable_helpdesk == 'Off'){
             <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="#">Security <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link" href="#">password</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Profile</a>
-                    </li>
+
                 </ul>
                 <form class="form-inline">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    <input class="form-control mr-sm-2" type="text" placeholder="" aria-label="Search">
                 </form>
             </div>
         </div>

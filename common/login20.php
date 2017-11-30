2ba4c63573760565bdc2ffb9e2d48e3744ee34cf
Peter Horrack <phorrack@casaria.net>
2017-11-29 16:49:35 -0800

    login screen woith modal dialogs

--- common/login20.php
+++ common/login20.php
@@ -135,9 +135,9 @@ if($enable_helpdesk == 'Off'){
                 <div class="modal-content">
 
                     <!--Header-->
-                    <div class="modal-header orange-gradient darken-3">
+                    <div class="modal-header orange-gradient darken-3  ">
                         <h4 class="title"><i class="fa fa-user-plus"></i> Create Secure Password</h4>
-                        <button type="button" class="close waves-effect waves-light" data-dismiss="modal" aria-label="Close">
+                        <button type="button" class="close waves-effect waves-light" >
                             <span aria-hidden="false">&times;</span>
                         </button>
                     </div>

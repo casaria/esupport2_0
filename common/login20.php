a0b770e829a132edfcfefda03e202fb9979d4f62
Peter Horrack <phorrack@casaria.net>
2017-12-02 00:54:37 -0800

    styles for login20

--- common/login20.php
+++ common/login20.php
@@ -518,7 +518,7 @@ if($enable_helpdesk == 'Off'){
     </nav>
 
     <!--Intro Section-->
-    <section class="view intro-6 .pattern7 hm-indigo-light">
+    <section class="view intro-6 .pattern7 hm-indigo-slight">
         <div class="full-bg-img flex-center">
             <div class="container">
                 <div class="row">

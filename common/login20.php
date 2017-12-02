43350d2c58fdf7c3b2242f90d4beba27d9b8f951
Peter Horrack <phorrack@casaria.net>
2017-12-02 07:19:34 -0800

    last commit before surgery

--- common/login20.php
+++ common/login20.php
@@ -89,17 +89,6 @@ function setSupporterCookie()
 
 
 
-    <!-- JQuery -->
-    <script type="text/javascript" src="../mdb/js/jquery-3.2.1.js"></script>
-    <!-- Bootstrap tooltips -->
-    <script type="text/javascript" src="../mdb/js/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="../mdb/js/bootstrap.min.js"></script>
-    <!-- MDB core JavaScript    +++   mdb.min -->
-    <script type="text/javascript" src="../mdb/js/mdb.js"></script>
-
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
-
 
     <style>
         .intro-1{
@@ -596,6 +585,18 @@ if($enable_helpdesk == 'Off'){
 
 
 <!-- SCRIPTS -->
+
+
+<!-- JQuery -->
+<script type="text/javascript" src="../mdb/js/jquery-3.2.1.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="../mdb/js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="../mdb/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript    +++   mdb.min -->
+<script type="text/javascript" src="../mdb/js/mdb.js"></script>
+
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
 <!-- SCRIPTS -->
 
 <script>

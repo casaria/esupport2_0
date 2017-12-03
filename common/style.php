2287667aea19fcfb661625fa087f5f74c7d50b89
Peter Horrack <phorrack@casaria.net>
2017-12-02 19:01:47 -0800

    After surgery

--- common/style.php
+++ common/style.php
@@ -59,6 +59,10 @@ $tablePadding =2;
     <!--  <script src="../js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous"> -->
+
+    <style>
+        * {margin: 0; padding: 0;}
+    </style>
     <link rel="stylesheet" href="/css/bootstrap.min.css" >
 
 

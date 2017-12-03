eb6720a1c8770e7efaf0f6162de2e3e3a74c7469
Peter Horrack <phorrack@casaria.net>
2017-12-02 16:35:55 -0800

    After surgery

--- common/login20.php
+++ common/login20.php
@@ -201,14 +201,6 @@ function setSupporterCookie()
 
     </style>
 
-    <!-- JQuery -->
-    <script src="../mdb/js/jquery-3.2.1.js"></script>
-    <!-- Bootstrap tooltips -->
-    <script src="../mdb/js/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script src="../mdb/js/bootstrap.min.js"></script>
-    <!-- MDB core JavaScript    +++   mdb.min -->
-    <script Src="../mdb/js/mdb.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
     <!-- SCRIPTS -->
@@ -676,6 +668,14 @@ echo
     </section>
     </header>
 
+    <!-- JQuery -->
+    <script src="../mdb/js/jquery-3.2.1.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script src="../mdb/js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script src="../mdb/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript    +++   mdb.min -->
+    <script Src="../mdb/js/mdb.js"></script>
     <script>
         new WOW().init();
     </script>

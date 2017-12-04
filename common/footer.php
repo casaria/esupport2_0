c6392ac813e70eb6b8b7f8ededc7e20c723ebab8
Peter Horrack <phorrack@casaria.net>
2017-12-04 04:04:17 -0800

    ob_start

--- common/footer.php
+++ common/footer.php
@@ -30,6 +30,7 @@
 			**		Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 			**
 			***************************************************************************************/
+require_once  $_SERVER['DOCUMENT_ROOT']."/common/common.php";
 
 $theme = getThemeVars(getThemeName('default'));
 

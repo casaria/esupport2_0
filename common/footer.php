21a39c91a0efa6f83318d589dbf2efeb73b6b76f
Peter Horrack <phorrack@casaria.net>
2017-12-04 03:57:50 -0800

    ob_start

--- common/footer.php
+++ common/footer.php
@@ -30,6 +30,7 @@
 			**		Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 			**
 			***************************************************************************************/
+require_once  $_SERVER['DOCUMENT_ROOT']."/common/config.php";
 
 
 echo "<br><center><font size=0 color=$theme[text] face=\"$theme[font]\">$helpdesk_name<br>";

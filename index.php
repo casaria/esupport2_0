b9ad59ff75ab9eef706a067f7af40119029a57b8
Peter Horrack <phorrack@casaria.net>
2017-12-04 19:16:59 -0800

    ob_start

--- index.php
+++ index.php
@@ -34,9 +34,9 @@
 $mtime1 = explode(" ", microtime());
 $starttime = $mtime1[0] + $mtime1[1];
 
-require_once "common/config.php";
-require_once "common/$database.class.php";
-require_once "common/common.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/common/config.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/common/mysql.class.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/common/common.php";
 
 startSession();
 

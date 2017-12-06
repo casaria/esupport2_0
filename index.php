6cef60cd0f834f35ab6db203921bb68a1a8764cd
Peter Horrack <phorrack@casaria.net>
2017-12-05 17:12:20 -0800

    login sessions and cookies

--- index.php
+++ index.php
@@ -38,15 +38,15 @@ require_once $_SERVER['DOCUMENT_ROOT']."/common/config.php";
 require_once $_SERVER['DOCUMENT_ROOT']."/common/mysql.class.php";
 require_once $_SERVER['DOCUMENT_ROOT']."/common/common.php";
 
-startSession();
-$cookie_name = $_COOKIE['cookie_name'];
-
 if($reg == 'yes'){
     require_once "lang/$default_language.lang.php";
     require "register.php";
     exit;
 }
 
+
+startSession();
+$cookie_name = $_COOKIE['cookieuser'];
 
 if($pubpriv == 'Private') {
     if (($session_id !== session_id()) || (!$cookie_name) || ($cookie_name == '')) {

5fb40bb467c09eda986546097174ddf70630dd07
Peter Horrack <phorrack@casaria.net>
2017-12-07 07:38:12 -0800

    deployed 1.5.2c - security https upgrade for cookies

--- common/common.php
+++ common/common.php
@@ -405,7 +405,7 @@ function isCookieSet($cookie_name, $enc_pwd)
 
 function startSession() {
 	global $session_time, $session_name, $cookie_name;
-    session_set_cookie_params($session_time,'/', '.casaria.net',0,1);
+    session_set_cookie_params($session_time,'/', '.casaria.net',1,1);
     session_name($session_name);
     session_start();
     if (!isset($_SESSION['count'])) {

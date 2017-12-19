c7eb42a2796f98c3ad3934299818d8ea17931c85
Peter Horrack <phorrack@casaria.net>
2017-12-19 15:19:39 -0800

    Transoition  to 1 5 3

--- common/footer.php
+++ common/footer.php
@@ -6,21 +6,19 @@
 **
 **	This file contains all footer information.
 **
-************************************************************************************************************
+***********************************************************************************************************/
 
 require_once  $_SERVER['DOCUMENT_ROOT']."/common/themelib.php";
 //$theme = getThemeVars(getThemeName('default'));
 
-
-echo '<br><div class="stats" align="center"> '.$helpdesk_name.<b> Database: $dbname'</b><br>';
+echo '<br><div class="stats" align="center"> '.$helpdesk_name.'<b> Database:'.$db_name.'</b><br>';
 echo "$lang_powered Peter & <b><a href=\"https://odoo.casaria.net\">TheTeam</a></b> V$version<br>";
-echo "<a href=\"https://icons8.com/icon/44052/Shopping-Cart-Loaded\"> icon credits</a><br>";
+echo "Aditional redits <a href=\"https://icons8.com/icon/44052/Shopping-Cart-Loaded\"> icon credits</a><br>";
 if($enable_stats == 'On'){
     $mtime2 = explode(" ", microtime());
     $endtime = $mtime2[0] + $mtime2[1];
 	$totaltime = $endtime - $starttime;
 	$totaltime = number_format($totaltime, 7);
-	
 	echo "$lang_processed: $totaltime $lang_seconds, $db->queries $lang_queries<br>";
 }
 echo "</div>";

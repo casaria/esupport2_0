41ec3a14dbc0174f12573c7b60e63e4b8409d198
Peter Horrack <phorrack@casaria.net>
2017-12-20 06:28:45 -0800

    session_start  headers already sent issue

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 
-<link rel="apple-touch-icon" sizes="120x120" href="/img/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-<link rel="manifest" href="/img/manifest.json">
-<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="theme-color" content="#ffffff"><?php
+<?php
 /**************************************************************************************************
 **	file:	index.php
 **
@@ -73,6 +68,14 @@ ob_end_clean();
 authenticate();
 RewindSession();
 
+?>
+<link rel="apple-touch-icon" sizes="120x120" href="/img/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+<link rel="manifest" href="/img/manifest.json">
+<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
+<?php
 $language = getLanguage($_SESSION['cookie_name']);
 if($language == '')
 	require_once "lang/$default_language.lang.php";

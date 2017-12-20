f47d8379a09ede4ed26f32c80c10d2116480eea6
Peter Horrack <phorrack@casaria.net>
2017-12-20 04:17:40 -0800

    QR Code test

--- supporter/tcreate.php
+++ supporter/tcreate.php
@@ -173,29 +173,17 @@ else {
                     createTicketInfo('allow', $ug); ?></P>
             </div>
             <div class="tab-pane fade" id="asset" role="tabpanel" aria-labelledby="asset-tab">
-                <p>
-                <div class="container">
-                    <div class="row">
-                        <div class="col align-self-start">
-                            One of three columns
-                        </div>
-                        <div class="col align-self-center">
-                            Share ticket<br>
-                            <?php createQRcode('https//casaria.net');
-                            /*     try {
-                                     Connection::get()->connect();
-                                     echo 'A connection to the PostgreSQL database sever has been established successfully.';
-                                 } catch (PDOException $e) {
-                                     echo $e->getMessage();
-                                 }    */
-                            ?>
-                        </div>
-                        <div class="col align-self-end">
-                            One of three columns
-                        </div>
-                    </div>
-                </div>
+                <p>Asset Database <br>
 
+                    <?php createQRcode('https//casaria.net');
+               /*     try {
+                        Connection::get()->connect();
+                        echo 'A connection to the PostgreSQL database sever has been established successfully.';
+                    } catch (PDOException $e) {
+                        echo $e->getMessage();
+                    }    */
+                    ?>
+                </p>
             </div>
         </div>
 

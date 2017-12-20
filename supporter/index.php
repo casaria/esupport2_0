ff6a87ae84708d9eb9591c1932df78b9da371775
Peter Horrack <phorrack@casaria.net>
2017-12-20 05:56:26 -0800

    Asset tab

--- supporter/index.php
+++ supporter/index.php
@@ -1,13 +1,4 @@
 
-
-<link rel="apple-touch-icon" sizes="120x120" href="/img/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-<link rel="manifest" href="/img/manifest.json">
-<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="theme-color" content="#ffffff">
-
-
 <?php
 /**************************************************************************************************
 **	file:	index.php
@@ -57,6 +48,15 @@ if($pubpriv == 'Private') {
 
 RewindSession();
 ?>
+
+
+<link rel="apple-touch-icon" sizes="120x120" href="/img/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+<link rel="manifest" href="/img/manifest.json">
+<link rel="mask-icon" href="/img/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
+
 
 <script type="text/javascript">
 function winPop(url)

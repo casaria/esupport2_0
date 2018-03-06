096264bd7af826f3c79ad2e15d255abd74ae6d1f
Peter Horrack <phorrack@casaria.net>
2018-03-06 10:14:04 -0700

    $cookie_name=''; rearranged in logout

--- common/logout.php
+++ common/logout.php
@@ -40,8 +40,12 @@ require_once "common.php";
 //if (isset($_COOKIE[session_name()])) {
   $cookie_name='';
     setcookie("session_id", session_id(), time() - $session_time, '/', 'casaria.net');
-    if (isset($_COOKIE['cookieuser'])) setcookie("cookieuser", $cookie_name, time() - 3600, '/', 'casaria.net');
-    if (isset($_COOKIE['supporter_usercookie'])) setcookie("supporter_usercookie", $cookie_name, time() - 3600, '/', 'casaria.net');
+    if (isset($_COOKIE['cookieuser'])) {
+        setcookie("cookieuser", $cookie_name, time() - 3600, '/', 'casaria.net');
+        unset($_COOKIE['cookieuser']);
+    }
+    if (isset($_COOKIE['supporter_usercookie']))
+        setcookie("supporter_usercookie", $cookie_name, time() - 3600, '/', 'casaria.net');
 
 //session_set_cookie_params(600,'/', '.casaria.net',0,0);
 

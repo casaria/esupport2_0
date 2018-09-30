f7e8ead4083277988392d30cd1075e1b918a8c69
Peter Horrack <phorrack@casaria.net>
2018-09-30 12:46:59 -0700

    time update extended by T-2 years

--- supporter/tupdate.php
+++ supporter/tupdate.php
@@ -840,6 +840,7 @@ function createTimeTab()
             <select class="tkt-date" name=woyear>';
         echo "<option value=" . (string)($today['year'] - 2);
         echo '>' . ($today['year'] - 2) . '</option>';
+
         echo "<option value=" . (string)($today['year'] - 1);
         echo '>' . ($today['year'] - 1) . '</option>';
 

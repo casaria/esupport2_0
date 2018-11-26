0e01590823ad8b9f9248b40ee019a8f0a68ad012
Peter Horrack <phorrack@casaria.net>
2018-11-26 01:49:23 -0800

    correted permissisons flags for new supporters to inlude user "1"

--- common/config.php
+++ common/config.php
@@ -91,7 +91,7 @@ if (preg_match('/supporter/i',$full_url)) {
     if ($cookie_name == '') $cookie_name = $_COOKIE['supporter_usercookie'];
 } else {
 
-    if ($cookie_name == '') $cookie_name =$_COOKIE['usercookie'];
+    if ($cookie_name == '') $cookie_name =$_COOKIE['cookieuser'];
 }
 
 $mysql_crmsettings_table = "crmsettings";
